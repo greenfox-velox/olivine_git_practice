45a02fa2205d88f596a474e107f2cfe8b7333b2c
David <doczi.david.daci@gmail.com>
2016-08-01 17:55:16 +0200

    a1

---
+++ Greet.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by david on 2016.08.01..
+ */
+public class Greet {
+    public void greet(){
+        System.out.println("This is greet method. Finally.");
+    }
+}
