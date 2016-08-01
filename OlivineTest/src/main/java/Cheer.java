0e0fd15bbf7a367bee91ea6785f15367110ee834
attilakrupl <krupl.attila@gmail.com>
2016-08-01 16:29:43 +0100

    project

--- OlivineTest/src/main/java/Cheer.java
+++ OlivineTest/src/main/java/Cheer.java
@@ -2,4 +2,10 @@
  * Created by Attila on 2016.08.01..
  */
 public class Cheer {
+
+    public String teamName = "Olivine";
+
+    public void printTeamName() {
+        System.out.println(this.teamName);
+    }
 }
