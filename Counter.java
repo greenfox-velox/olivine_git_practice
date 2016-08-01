b6308493c093afc76b2c1b5630bae46a141d2d2f
timikurucz <kurucztm@gmail.com>
2016-08-01 18:01:34 +0200

    b2

--- Counter.java
+++ Counter.java
@@ -4,10 +4,9 @@ package test;
  * Created by timi.kurucz on 2016.08.01..
  */
 public class Counter {
-    public int age = 29;
 
-    public void printAge(){
-        System.out.println(this.age);
+    public void printAge(int age){
+        System.out.println(age);
     }
 
 }
