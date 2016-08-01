50c3cee0a10cdf9acc8056be5546da42d565b72c
attilakrupl <krupl.attila@gmail.com>
2016-08-01 16:54:03 +0100

    C1

---
+++ Cheer.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Created by Attila on 2016.08.01..
+ */
+public class Cheer {
+
+    public String teamName = "Olivine";
+
+    public void printTeamName() {
+        System.out.println(this.teamName);
+    }
+
+}
